df16f27c6f7e61ca9b29bdf91f251dfb33fd718a
zhuguanghui <fai0912@163.com>
2023-03-13 11:23:16 +0800

    提交2

--- test1/src/main/java/com/zgh/demo/Demo1.java
+++ test1/src/main/java/com/zgh/demo/Demo1.java
@@ -1,9 +1,22 @@
 package com.zgh.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("OK");
         System.out.println("Test");
         System.out.println("Yes");
+        int a = 10;
+        while (true) {
+            if (a > 0) {
+                System.out.println(a);
+                a--;
+            }else {
+                return;
+            }
+        }
     }
+
 }
